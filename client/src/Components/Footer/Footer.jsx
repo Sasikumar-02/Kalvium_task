30e73cd42c99be07c5219456c1b022e1ed66e94e
Sasi Kumar M <sasikumarmurugan02@gmail.com>
2023-04-24 21:50:53 +0530

    first commit

---
+++ client/src/Components/Footer/Footer.jsx
@@ -0,0 +1,142 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const Footer = () => {
+  return (
+    <div className="footer xl:mt-12 bg-slate-400 tracking-widest">
+      <div className="footer-main w-full xl:w-4/5 xl:m-auto mb-2 p-4 xl:flex">
+        <div className="flex w-full justify-between xl:w-6/12">
+          <div className="footer-col-1 w-6/12 xl:w-full space-y-4">
+            <h3
+              className="text-xl uppercase p-1 border-b-2 border-white w-fit my-2"
+              style={{
+                width: "fit-content",
+                color: "black",
+                fontWeight: "bolder",
+              }}
+            >
+              MERN-Quiz-App
+            </h3>
+            <p className="text-white text-xs pr-4 xl:text-sm">
+              WHERE PASSION MEETS PERFECTION !
+            </p>
+          </div>
+          <div className="footer-col-2 w-6/12 xl:w-full mr-2 md:mr-0 space-y-4">
+            <h3
+              className="text-white text-xl uppercase p-1 border-b-2 border-white w-fit my-2"
+              style={{
+                width: "fit-content",
+                color: "black",
+                fontWeight: "bolder",
+              }}
+            >
+              quiz
+            </h3>
+            <Link
+              to="/"
+              className="no-underline text-white text-xs block xl:text-sm"
+            >
+              All Quiz
+            </Link>
+          </div>
+        </div>
+        <div className="flex w-full justify-between my-4 xl:w-6/12 xl:my-0">
+          <div className="footer-col-3 w-6/12 xl:w-full space-y-4">
+            <h3
+              className="text-white text-xl uppercase p-1 border-b-2 border-white w-fit my-2"
+              style={{
+                width: "fit-content",
+                color: "black",
+                fontWeight: "bolder",
+              }}
+            >
+              follow
+            </h3>
+            <div className="flex items-center">
+              <i className="fa fa-instagram text-white mr-1"></i>
+              <a
+                href="https://sasikumar-02.github.io/"
+                className="no-underline text-white text-xs block xl:text-sm"
+                target="_blank"
+              >
+                Portfolio
+              </a>
+            </div>
+            <div className="flex items-center">
+              <i className="fa fa-linkedin text-white mr-1"></i>
+              <a
+                href="https://www.linkedin.com/in/sasi-kumar-m-077212210"
+                className="no-underline text-white text-xs block xl:text-sm"
+                target="_blank"
+              >
+                LinkedIn
+              </a>
+            </div>
+            <div className="flex items-center">
+              <i className="fa fa-facebook text-white mr-1"></i>
+              <a
+                href="https://github.com/Sasikumar-02"
+                className="no-underline text-white text-xs block xl:text-sm"
+                target="_blank"
+              >
+                Github
+              </a>
+            </div>
+            <div className="flex items-center">
+              <i className="fa fa-facebook text-white mr-1"></i>
+              <a
+                href="https://leetcode.com/sasi_kumar_m/"
+                className="no-underline text-white text-xs block xl:text-sm"
+                target="_blank"
+              >
+                Leetcode
+              </a>
+            </div>
+          </div>
+          <div className="footer-col-4 w-6/12 xl:w-full space-y-4 mr-2 md:mr-0">
+            <h3
+              className="text-white text-xl uppercase p-1 border-b-2 border-white w-fit my-2"
+              style={{
+                width: "fit-content",
+                color: "black",
+                fontWeight: "bolder",
+              }}
+            >
+              Contact
+            </h3>
+            <p className="flex items-center my-1">
+              <i
+                className="fa fa-home text-white text-sm"
+                style={{ marginRight: "2px" }}
+              ></i>
+              <span className="text-white text-xs xl:text-sm">
+                Dharmapuri, Tamilnadu
+              </span>
+            </p>
+            <p className="flex items-center my-1">
+              <i
+                className="fa fa-envelope text-white text-xs"
+                style={{ marginRight: "2px" }}
+              ></i>
+              <span className="text-white text-xs xl:text-sm">
+                sasikumarm.cs20@bitsathy.ac.in
+              </span>
+            </p>
+            <p className="flex items-center my-1">
+              <i
+                className="fa fa-phone text-white"
+                style={{ marginRight: "2px" }}
+              ></i>
+              <span className="text-white text-xs xl:text-sm">
+                +91 9789113517
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+      <p className="text-white text-center mt-4 pb-4">
+        MERN Quiz App Designed and Developed By Sasi Kumar M.
+      </p>
+    </div>
+  );
+};
